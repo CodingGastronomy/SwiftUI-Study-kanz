fac4f9f2fe4476e2cc412bfe67e0c63b9af1d2dc
Kanz <87kangsw@gmail.com>
2020-08-20 01:30:33 +0900

    # Section 12 - DragGesture

--- TapGetstureTest/TapGetstureTest/ContentView.swift
+++ TapGetstureTest/TapGetstureTest/ContentView.swift
@@ -9,15 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isTapped: Bool = false
+    @State private var cardDragState: CGSize = .zero
     
     var body: some View {
         Card(isTapped: self.$isTapped)
-            .onTapGesture {
-                print("Tapped")
-                withAnimation {                
-                    self.isTapped.toggle()
-                }
-            }
+            .offset(y: self.cardDragState.height)
+            .animation(.spring())
+            .gesture(TapGesture(count: 1)
+                        .onEnded({ _ in
+                            print("Tapped")
+                            withAnimation {
+                                self.isTapped.toggle()
+                            }
+                        }))
+            
+            .gesture(DragGesture()
+                        .onChanged({ value in
+                            print(value.translation)
+                            self.cardDragState = value.translation
+                        })
+                        .onEnded({ value in
+                            self.cardDragState = .zero
+                        })
+            )
+        
     }
 }
 

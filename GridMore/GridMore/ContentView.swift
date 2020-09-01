cab035ad06afd39a12bd025a45f1cd855add179a
Kanz <87kangsw@gmail.com>
2020-09-02 02:23:47 +0900

    # Section 27 Grid More..(2)

--- GridMore/GridMore/ContentView.swift
+++ GridMore/GridMore/ContentView.swift
@@ -10,23 +10,44 @@ import SwiftUI
 struct ContentView: View {
     
     let columns: [GridItem] = [
-        GridItem(.fixed(100)),
-        GridItem(.fixed(150)),
-        GridItem(.fixed(100))
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
     ]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: self.columns) {
-                
-                ForEach(1..<20) { _ in
-                    Rectangle()
-                        .fill(Color.red)
-                        .aspectRatio(contentMode: .fit)
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: self.columns, spacing: 10, pinnedViews: [.sectionHeaders]) {
+                    
+                    ForEach(1..<11, id: \.self) { index in
+                        
+                        Section(header: self.headerView(index)) {
+                            ForEach(1..<20) { _ in
+                                Rectangle()
+                                    .fill(Color.red)
+                                    .aspectRatio(contentMode: .fit)
+                                    .id(UUID())
+                            }
+                        }
+                    }
                 }
+                .padding()
             }
+            
+            .navigationTitle("Pinned Views")
         }
     }
+    
+    private func headerView(_ index: Int) -> some View {
+        Text("Section: \(index)")
+            .padding()
+            .foregroundColor(Color.white)
+            .font(.title)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

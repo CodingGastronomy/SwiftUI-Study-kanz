c01b89c161b2a99fab6bc56790fe75d51d6959ea
Kanz <87kangsw@gmail.com>
2020-09-04 00:32:29 +0900

    # Section 27 Keyboard

---
+++ PreventKeyboard/PreventKeyboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreventKeyboard
+//
+//  Created by Kanz on 2020/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreventKeyboard/PreventKeyboard/PreventKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreventKeyboardApp.swift
+//  PreventKeyboard
+//
+//  Created by Kanz on 2020/09/04.
+//
+
+import SwiftUI
+
+@main
+struct PreventKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
